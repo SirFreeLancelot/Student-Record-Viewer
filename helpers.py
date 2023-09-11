f514ae9baa383a64a778c1471efa4ebecaea8b93
SirFreeLancelot <spare1412@gmail.com>
2023-09-11 20:01:27 +0530

    v1.4 ready to deploy

--- helpers.py
+++ helpers.py
@@ -9,7 +9,7 @@ sheet_name = "Student Record Manager"
 batch_sessions = ["Practical", "AETCOM"]
 cutoff = {"Theory": 75 ,"Practical": 80, "AETCOM": 75}
 
-# Keys must match possible batch names
+# Frames must match possible batch ranges
 dataframes = ["D2:GU52", "D55:GU105", "D108:GU158", "D161:GU211", "D214:GU264"]
 theory_frame = "D1:GU251"
 scores_frame = "B2:Z253"
@@ -23,12 +23,14 @@ practical_scores = ['Practical 1','Practical 2','Practical 3','Record',
                     'Skill Certification','ECE','Assignment','Pr Professionalism']
 
 
+# Cache google sheet credentials
 @st.cache_resource
 def load_google_sheets_credentials():
     google_sheets_credentials = st.secrets["google_sheets_credentials"]
     return google_sheets_credentials
 
 
+# Cache gspread
 @st.cache_resource
 def authorize_client():
     google_sheets_credentials = load_google_sheets_credentials()
@@ -44,11 +46,13 @@ def authorize_client():
     return gspread.authorize(credentials)
 
 
+# Convert dataframes to pandas dataframes with first row as column names
 def df_with_header(data):
     df = pd.DataFrame(data=data[1:], columns=data[0])
     return df
 
 
+# Load student data into session state variables
 def load_student_data():
     try:
         client = authorize_client()
@@ -69,29 +73,31 @@ def load_student_data():
         return False
     
 
+# Display eligibility criteria for attendance
 def attendance_eligibility_criteria():
     with st.expander("Eligibility Criteria"):
         st.write(f''' 1. Theory Attendance must be at least {cutoff['Theory']}%. 
-                    This includes theory classes ''')
+                    This includes only theory classes. ''')
         st.write(f''' 2. Practical Attendance must be at least {cutoff['Practical']}%. 
                     This includes lab sessions, SGDs, ECEs, Skill Certifications.
-                    Each session is worth 2 hours, except ECE, which is worth 3 hours. ''')
+                    Each session is worth 2 hours, except ECE which is worth 3 hours. ''')
         st.write(f''' 3. AETCOM Attendance must be at least {cutoff['AETCOM']}%. 
-                    This includes AETCOM sessions ''')
+                    This includes only AETCOM sessions, each worth 2 hours. ''')
         st.write(f''' Key: SGD - Small Group Discussions; ECE - Early Clinical Exposure; 
-                      AETCOM - Attitude, Ethics and Communication ''')
+                      AETCOM - Attitude, Ethics and Communication. ''')
         
 
+# Display eligibility criteria for scores
 def scores_eligibility_criteria():
     with st.expander("Eligibility Criteria"):
         st.write(f''' 1. Theory Total must be at least 40%. 
                     This includes average of the three theory internal scores scaled to 80, 
-                    and other formative scores scaled to 20 ''')
+                    and other formative scores scaled to 20. ''')
         st.write(f''' 2. Practical Total must be at least 40%. 
                     This includes average of the three practical internal scores scaled to 80,
-                    and other formative scores scaled to 20 ''')
+                    and other formative scores scaled to 20. ''')
         st.write(f''' 3. Aggregate Score must be at least 50%. 
-                    This is the average of the theory and practical scores ''')
+                    This is the average of the theory and practical scores. ''')
     with st.expander("Scoring Criteria"):
         st.write(f''' 1. Aggregate Score (max. 100) = (Theory Total + Practical Total) / 2 ''')
         st.write(f''' 2. Theory Total (max. 100) = Theory IA + Theory FA ''')
@@ -107,11 +113,12 @@ def scores_eligibility_criteria():
         st.write(f''' Key: IA - Internal Assessment; FA - Formative Assessment; MCQ - Multiple Choice Questions''')
 
 
+# Render theory attendance
 def render_theory(roll_number):
     theory = st.session_state.theory
     date_columns = list(theory.columns)
     if date_columns[0] == 'E':
-        st.write(f"Theory : No theory classes conducted")
+        st.write(f"Theory : No theory classes conducted yet")
         return
     att_str = ''
     abs_list = [f'(s.no. | dd-mm | time)']
@@ -153,17 +160,18 @@ def render_theory(roll_number):
         st.error(f'{errors} errors detected in theory records. Kindly notify the department office', icon="⚠️")
 
 
+# Render Practical and AETCOM attendance
 def render_attendance(roll_number):
     for batch in batch_sessions:        
         data = st.session_state[batch][(roll_number-1) // 50]
         date_columns = list(data.columns)
         if date_columns[0] == 'E':
-            st.write(f"{batch} : No {batch} sessions conducted")
+            st.write(f"{batch} : No {batch} sessions conducted yet")
             continue
         att_str = ''
-        abs_list = ['(s.no. - dd-mm | time)']
+        abs_list = ['(s.no. | dd-mm | time)']
         if batch == 'Practical':
-            abs_list = ['(s.no. - dd-mm | time | session)']
+            abs_list = ['(s.no. | dd-mm | time | session)']
         total = 0
         attended = 0
         errors = 0
@@ -216,11 +224,16 @@ def render_attendance(roll_number):
             st.error(f'{errors} errors detected in {batch} records. Kindly notify the department office', icon="⚠️")
 
 
+# Render scores
 def render_scores(roll_number):
     scores = st.session_state.scores
+    
+    # Load formatted scores of student into dictionary
     student_scores = {}
     for column in scores_columns:
         student_scores[column] = f'{scores[column][roll_number-1]} / {scores[column][250]}'
+    
+    # Check eligibility
     if int(scores['Aggregate'][roll_number-1]) < 50:
         st.session_state.eligible = False
         aggregate_eligibility = '🔴 :red[Not Eligible]'
@@ -236,17 +249,29 @@ def render_scores(roll_number):
         practical_eligibility = '🔴 :red[Not Eligible]'
     else:
         practical_eligibility = '🟢 :green[Eligible]'
-    st.write(f"1. Theory Total : {student_scores['Theory Total']} ( {theory_eligibility} )")
-    st.write(f"2. Practical Total : {student_scores['Practical Total']} ( {practical_eligibility} )")
-    st.write(f"3. Aggregate Score : {student_scores['Aggregate']} ( {aggregate_eligibility} )")
-    with st.expander("Breakdown of Theory Scores"):
+
+    # Show eligibility at the year end
+    #st.write(f"1. Theory Total : {student_scores['Theory Total']} ( {theory_eligibility} )")
+    #st.write(f"2. Practical Total : {student_scores['Practical Total']} ( {practical_eligibility} )")
+    #st.write(f"3. Aggregate Score : {student_scores['Aggregate']} ( {aggregate_eligibility} )")
+
+    # Display final scores
+    with st.expander("Final Scores for Eligibility"):
+        st.write("1. Theory Total : TBD")
+        st.write("2. Practical Total : TBD")
+        st.write("3. Aggregate Score : TBD")
+    
+    # Display theory scores
+    with st.expander("Theory Scores"):
         for column in theory_scores:
             zeroes = scores[column].tolist().count('0')
             if zeroes == 250:
                 st.write(f"{column} : TBD")
             else:
                 st.write(f"{column} : {student_scores[column]}")
-    with st.expander("Breakdown of Practical Scores"):
+    
+    # Display practical scores
+    with st.expander("Practical Scores"):
         for column in practical_scores:
             zeroes = scores[column].tolist().count('0')
             if zeroes == 250:
@@ -255,15 +280,17 @@ def render_scores(roll_number):
                 st.write(f"{column} : {student_scores[column]}")
 
 
+# Render signatures
 def signatures():
     sign1, sign2 = st.columns([2,1])
 
     with sign1:
         st.success(" Developed by Dr Suraj", icon="🌟")
     with sign2:
-        st.info(" Version 1.2", icon="ℹ️")
+        st.info(" Version 1.4", icon="ℹ️")
 
 
+# Render disclaimers
 def disclaimers():
     st.write('''** Please note that these records are provided provisionally 
              for your reference, by the Department of Physiology. 
@@ -279,6 +306,7 @@ def disclaimers():
              Not fulfilling the criteria will result in disqualification.''')
 
 
+# Developer's Note
 def developers_note():
     st.write('''** Developer's Note - Thank you for using my app! I hope you find it helpful. 
              I would really appreciate your valuable feedback. Anything you would like to tell me is welcome!
--- srv.py
+++ srv.py
@@ -2,26 +2,28 @@ import streamlit as st
 from helpers import *
 from datetime import date
 
+# Title of the app
 st.title('Student Record Viewer')
 
+# Instructions
 st.info(''' Enter your Roll Number''', icon="ℹ️")
 
+# Initialize session state variables
 if 'valid_roll_number' not in st.session_state:
     st.session_state.valid_roll_number = False
-
 if 'data_pulled' not in st.session_state:
     st.session_state.data_pulled = False
-
 if 'data_date' not in st.session_state:
     st.session_state.data_date = ''
-
 if 'eligible' not in st.session_state:
     st.session_state.eligible = False
 
+# Roll Number input and space for warning
 rno, warn = st.columns([1,5])
 with rno:
     roll_number = st.text_input("Roll Number")
 
+# Validate Roll Number
 try:
     roll_number = int(roll_number)
     if roll_number not in range(1, 251):
@@ -32,6 +34,7 @@ except:
         st.warning(' Roll Number must be between 1 to 250', icon="⚠️")
     st.session_state.valid_roll_number = False
 
+# Fetch data
 if st.session_state.valid_roll_number and not st.session_state.data_pulled:
     with st.spinner(''' Fetching student records. This happens only once and may take a few seconds.
                     Once the data is fetched, your subsequent checks will be instantaneous. '''):
@@ -48,25 +51,34 @@ if st.session_state.valid_roll_number and not st.session_state.data_pulled:
 
                     - Dr Suraj, your friendly neighborhood web developer. ''', icon="ℹ️")
 
+# Columns for flashing messages
 info1, info2 = st.columns([1,1])
 
+# Success message if data pulled
 if st.session_state.data_pulled:
     with info1:
         st.success(f' Fetched records as on {st.session_state.data_date}', icon="✅")
 
+# Show student record for roll number
 if st.session_state.valid_roll_number and st.session_state.data_pulled:
     with info2:
         st.info(f' Showing records for roll number {roll_number}', icon="ℹ️")
     st.session_state.eligible = True
+    
+    # Attendance block
     st.warning('Attendance') 
     attendance_eligibility_criteria()
     render_theory(roll_number)
     render_attendance(roll_number)
      
+    # Scores block
     st.warning('Scores')
+    # Scores update news
+    st.write(''' No assessments have been conducted yet. Check back here for updates as they come. ''')
     scores_eligibility_criteria()
     render_scores(roll_number)
 
+    # Celebrate if eligible
     if st.session_state.eligible:
         with warn:
             st.success('All eligibility criteria met! All the best for the exam!', icon="🌟")
